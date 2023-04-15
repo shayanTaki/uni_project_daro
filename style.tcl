5b182a9217f255e66e1af9498c8a28bd231670a6
shayan taki <76600102+shayanTaki@users.noreply.github.com>
2023-04-15 08:34:20 +0330

    one

---
+++ style.tcl
@@ -0,0 +1,3 @@
+ttk::style configure TButton -background black -foreground black -font "Helvetica 14" -padding "5 5"
+ttk::style configure TEntry -background black -foreground black -font "Helvetica 14" -fieldbackground "#111111"
+ttk::style configure TLabel -background black -foreground white -font "Helvetica 14"
